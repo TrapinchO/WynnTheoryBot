439c09ad281cc1b7e8567bce946d0e86a0fa0d3d
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2021-08-01 21:21:51 +0200

    Updated link for Twains in "oldlore" command

--- src/main/kotlin/cz/trapincho/wynntheorybot/commands/OldloreCommand.kt
+++ src/main/kotlin/cz/trapincho/wynntheorybot/commands/OldloreCommand.kt
@@ -15,7 +15,7 @@ class OldloreCommand : Command() {
                 .setTitle("Old lore links")
                 .setDescription("Links for the old archived lore")
                 .addField("Bob", "https://web.archive.org/web/20140408052809/http://forums.wynncraft.com/threads/wynn-tales-the-legend-of-bob.381", false)
-                .addField("Twains", "https://wynncraft.fandom.com/wiki/The_House_of_Twain_(Lore)?redirect=no", false)
+                .addField("Twains", "https://web.archive.org/web/20140410011458/http://forums.wynncraft.com/threads/wynn-tales-the-noble-house-of-twain.8072/", false)
                 .addField("Nether beast", "https://forums.wynncraft.com/threads/nether-beast.186210/", false)
                 .addField("Old storyline", "https://wynncraft.fandom.com/wiki/Storyline?redirect=no", false)
                 .build()
