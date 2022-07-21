ec9e24c4d6397136980a8a67f679260bedbba715
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2022-07-21 22:51:08 +0200

    added item rework

--- src/main/kotlin/cz/trapincho/wynntheorybot/events/SlashCommandEvent.kt
+++ src/main/kotlin/cz/trapincho/wynntheorybot/events/SlashCommandEvent.kt
@@ -69,6 +69,7 @@ private val reworkButtons = listOf(
     Button.link("https://forums.wynncraft.com/threads/jungle-lore-rework.290103/", "Jungle"),
     Button.link("https://forums.wynncraft.com/threads/the-ocean-lore-rework.292151/", "Ocean"),
     Button.link("https://forums.wynncraft.com/threads/gavel-lore-rework.295429/", "Gavel"),
+    Button.link("https://forums.wynncraft.com/threads/item-lore-rework.301483/", "Item"),
 )
 
 private val oldloreButtons = listOf(
